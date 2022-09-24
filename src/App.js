9f41491aba0f92f0ec6fc1316c5dc9c0365da9cf
Evgeniy <evgeniy_strel@yandex.ru>
2022-09-24 19:29:24 +0500

    wrote counter logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onClickPlus = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const onClickMinus = () => {
+    setCount(prevCount => prevCount - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>- Минус</button>
+        <button className="plus" onClick={onClickPlus}>Плюс +</button>
       </div>
     </div>
   );
