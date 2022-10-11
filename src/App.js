6e74dfe73a880bc374f549f1be5d8aad83ee65df
Evgeniy <82458628+evgeniy-strel@users.noreply.github.com>
2022-10-11 13:07:28 +0500

    closing modal on click on overlay

--- src/App.js
+++ src/App.js
@@ -2,28 +2,43 @@ import React from 'react';
 import './index.scss';
 
 function App() {
-  const [isOpenWindow, setIsOpenWindow] = React.useState(false)
+  const [isOpenWindow, setIsOpenWindow] = React.useState(false);
 
   const openWindow = () => {
     setIsOpenWindow(true);
-  }
+  };
 
   const closeWindow = () => {
     setIsOpenWindow(false);
-  }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('click', (e) => {
+      const overlay = document.querySelector('.overlay');
+      if (e.target == overlay) closeWindow();
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <button className="open-modal-btn" onClick={openWindow}>✨ Открыть окно</button>
-      {<div className={`${isOpenWindow && 'show'} overlay animated`}>
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200" onClick={closeWindow}>
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+    <div className='App'>
+      <button className='open-modal-btn' onClick={openWindow}>
+        ✨ Открыть окно
+      </button>
+      {
+        <div className={`${isOpenWindow && 'show'} overlay animated`}>
+          <div className='modal'>
+            <svg
+              height='200'
+              viewBox='0 0 200 200'
+              width='200'
+              onClick={closeWindow}>
+              <title />
+              <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
+            </svg>
+            <img src='papich-vibe.gif' />
+          </div>
         </div>
-      </div> }
+      }
     </div>
   );
 }
